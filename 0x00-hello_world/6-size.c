0ec2581b667ded84c8f75b648167bcad2215fed3
Miritigacheri <gacherimiriti3@gmail.com>
2023-08-10 19:26:40 -0700

    values

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - function used to return the values
+ * Return: 0 if succesful
+ *
+ */
+int main(void)
+{
+printf("Size of char: %lu byte(s)". sizeof(char));
+printf("Size of int: %lu byte(s)". sizeof(int));
+printf("Size of a long int: %lu byte(s)". sizeof(long int));
+printf("Size of long long int: %lu byte(s)". sizeof(long long int));
+printf("Size of float: %lu byte(s)". sizeof(float));
+return (0);
+}

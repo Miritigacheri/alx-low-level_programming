c32ad4784d90bb2bd6e581f5d0394ad95ce5df2a
Miritigacheri <gacherimiriti3@gmail.com>
2023-10-05 07:14:00 -0700

    more realloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include"main.h"
+#include<stdio.h>
+/**
+ **malloc_checked - allocates memory using malloc
+ *
+ *@b:number of bytes allocated
+ *
+ *Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+
+	}
+	else
+		return (ptr);
+}
